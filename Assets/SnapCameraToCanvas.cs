26ce0c6277097beab7b5e1417dd1e93fdbd6b4ed
James Veugelaers <james.veugelaers@outlook.co.nz>
2019-08-06 21:57:23 +1200

    Updated readme

--- Assets/SnapCameraToCanvas.cs
+++ Assets/SnapCameraToCanvas.cs
@@ -36,28 +36,32 @@ public class SnapCameraToCanvas : MonoBehaviour
         camera.farClipPlane = Mathf.Ceil(adjacent);
     }
 
-    private void OnDrawGizmos()
+    /*private void OnGUI()
     {
-        // Snap
-        SnapToCanvas();
-        
-        // Draw pretty lines for decoration
-        Vector3 canvasPos = canvas.transform.position;
-        
-        float fov = camera.fieldOfView / 2.0f;
-        float canvasHeight = ((RectTransform)canvas.transform).rect.height * canvas.transform.lossyScale.y;
+        if (GUI.Button("Align Camera"))
+        {
+            // Snap
+            SnapToCanvas();
 
-        // SOH CAH (TOA)
-        float opposite = canvasHeight / 2;
-        float adjacent = opposite / Mathf.Tan(Mathf.Deg2Rad * fov);
-        
-        Gizmos.color = Color.green; // Vertical
-        Gizmos.DrawLine(canvasPos, canvasPos + canvas.transform.up * opposite);
-        
-        Gizmos.color = Color.red; // Horizontal
-        Gizmos.DrawLine(canvasPos, canvasPos + -canvas.transform.forward * adjacent);
-        
-        Gizmos.color = Color.blue; // Connects top of Vertical to tip of Horizontal
-        Gizmos.DrawLine(canvasPos + canvas.transform.up * opposite, canvasPos + -canvas.transform.forward * adjacent);
-    }
+            // Draw pretty lines for decoration
+            Vector3 canvasPos = canvas.transform.position;
+
+            float fov = camera.fieldOfView / 2.0f;
+            float canvasHeight = ((RectTransform) canvas.transform).rect.height * canvas.transform.lossyScale.y;
+
+            // SOH CAH (TOA)
+            float opposite = canvasHeight / 2;
+            float adjacent = opposite / Mathf.Tan(Mathf.Deg2Rad * fov);
+
+            Gizmos.color = Color.green; // Vertical
+            Gizmos.DrawLine(canvasPos, canvasPos + canvas.transform.up * opposite);
+
+            Gizmos.color = Color.red; // Horizontal
+            Gizmos.DrawLine(canvasPos, canvasPos + -canvas.transform.forward * adjacent);
+
+            Gizmos.color = Color.blue; // Connects top of Vertical to tip of Horizontal
+            Gizmos.DrawLine(canvasPos + canvas.transform.up * opposite,
+                canvasPos + -canvas.transform.forward * adjacent);
+        }
+    }*/
 }
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# UnitySnapCameraToCanvas-
+# UnitySnapCameraToCanvas
 Script that aligns a Camera to a World Canvas
+
+![](assets/readme_align.gif)
+
+
+###### How To Use:
+Add SnapCameraToCanvas.cs to a component in your scene.
+Apply World Canvas to Canvas variabl.
+Run game
+See the align has snapped to the canvas.
